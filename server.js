6ca961266472e0086206a86e5a488abedf122963
Gerald Hero Hug <geraldhug92@gmail.com>
2020-02-14 14:23:49 +0800

    extend

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/auth', require('./route/auth'))
 
 const path = require('path')
 // Serve static assets in production
-if (process.env.NODE_ENB === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
